94df30653ed15cf29bbd695dce8d8890a4850983
JorgEdu1 <jorge.eduardo@alu.ufc.br>
2023-03-29 11:27:24 -0300

    instalando dependencias para rodar react no linux

--- code/src/App.js
+++ code/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 //import Dados from "./components/01MeusDados"; 
 //import Dados from "./components/02MeusDados"; 
